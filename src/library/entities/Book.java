f4ac13704e11465f3a320648a1f2ceafc84642f0
Jaan Liiband <jaan@liiband.com>
2015-09-23 14:02:52 +1000

    created the ctor and validateConstructorObject methods
    The validateConstructorObject method is capable of checking that objects
    passed to the ctor are valid if they are not it will throw an exception

--- src/library/entities/Book.java
+++ src/library/entities/Book.java
@@ -1,11 +1,50 @@
 package library.entities;
 
+import java.util.Date;
+
 import library.interfaces.entities.EBookState;
 import library.interfaces.entities.IBook;
 import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
+ 
 
 public class Book implements IBook {
 
+    private String author;
+    private String title;
+    private String callNumber;
+    private int bookID;
+    
+    
+    public Book(String author, String title, String callNumber, int bookID) {
+     
+        this.validateConstructorObject(author);
+        this.validateConstructorObject(title);
+        this.validateConstructorObject(callNumber);
+        this.validateConstructorObject(bookID);
+        
+        this.author = author;
+        this.title = title;
+        this.callNumber = callNumber;
+        this.bookID = bookID;
+        
+    }
+    
+    private void validateConstructorObject(Object object) throws IllegalArgumentException {
+        if(object instanceof String) {
+            String object_string = (String)object;
+            if(object_string == null || object_string.isEmpty()) {
+                throw new IllegalArgumentException("String cannot be null or empty");
+            }   
+        }
+        else if(object instanceof Integer) {
+            Integer object_integer = (Integer)object;
+            if(object_integer <= 0) {
+                throw new IllegalArgumentException("Integer cannot be less than or equal to 0");
+            }
+        }
+    }
+    
     @Override
     public void borrow(ILoan loan) {
         // TODO Auto-generated method stub
@@ -56,7 +95,6 @@ public class Book implements IBook {
 
     @Override
     public EBookState getState() {
-        // TODO Auto-generated method stub
         return null;
     }
 
