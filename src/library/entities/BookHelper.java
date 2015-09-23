56531a7cc39cd326c70d4f882aaab3aad5c96940
Jaan Liiband <jaan@liiband.com>
2015-09-23 15:16:18 +1000

    Added the BookHelper Class

---
+++ src/library/entities/BookHelper.java
@@ -0,0 +1,15 @@
+package library.entities;
+
+import library.interfaces.daos.IBookHelper;
+import library.interfaces.entities.IBook;
+
+public class BookHelper implements IBookHelper {
+
+    @Override
+    public IBook makeBook(String author, String title, String callNumber,
+            int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
