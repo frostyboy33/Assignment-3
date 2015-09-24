8b4251fecb4088707140f5a2252bc42e7e822f86
Jaan Liiband <jaan@liiband.com>
2015-09-24 10:01:20 +1000

    Added the library.testers package
    Added the TestBook class

---
+++ src/library/testers/TestBook.java
@@ -0,0 +1,107 @@
+package library.testers;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import library.interfaces.entities.IBook;
+
+public class TestBook {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+
+    @Test
+    public void testBook() {
+        fail("Not yet implemented");
+    }
+
+
+
+    @Test
+    public void testBorrow() {
+        fail("Not yet implemented");
+    }
+
+
+
+    @Test
+    public void testGetLoan() {
+        fail("Not yet implemented");
+    }
+
+
+
+    @Test
+    public void testReturnBook() {
+        fail("Not yet implemented");
+    }
+
+
+
+    @Test
+    public void testLose() {
+        fail("Not yet implemented");
+    }
+
+
+
+    @Test
+    public void testRepair() {
+        fail("Not yet implemented");
+    }
+
+
+
+    @Test
+    public void testDispose() {
+        fail("Not yet implemented");
+    }
+
+
+
+    @Test
+    public void testGetState() {
+        fail("Not yet implemented");
+    }
+
+
+
+    @Test
+    public void testGetAuthor() {
+        fail("Not yet implemented");
+    }
+
+
+
+    @Test
+    public void testGetTitle() {
+        fail("Not yet implemented");
+    }
+
+
+
+    @Test
+    public void testGetCallNumber() {
+        fail("Not yet implemented");
+    }
+
+
+
+    @Test
+    public void testGetID() {
+        fail("Not yet implemented");
+    }
+
+}
