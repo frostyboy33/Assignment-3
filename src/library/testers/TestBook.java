65e42d16f6b889fa4144ebf07d15ece89657473e
Jaan Liiband <jaan@liiband.com>
2015-09-24 10:34:16 +1000

    implementing Tests for the CTOR

--- src/library/testers/TestBook.java
+++ src/library/testers/TestBook.java
@@ -6,25 +6,61 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import library.entities.Book;
 import library.interfaces.entities.IBook;
 
 public class TestBook {
 
+    IBook book;
+    
+    private final String AUTHOR = "author";
+    private final String TITLE = "title";
+    private final String CALL_NUMBER = "11AC";
+    private final Integer BOOK_ID = 1; 
+    
     @Before
     public void setUp() throws Exception {
+        this.book = new Book(this.AUTHOR, 
+                             this.TITLE, 
+                             this.CALL_NUMBER, 
+                             this.BOOK_ID);
     }
 
 
 
     @After
     public void tearDown() throws Exception {
+        this.book = null;
     }
 
 
-
+    @Test(expected = IllegalArgumentException.class)
+    public void testBookIllegalArgumentExceptionAuthor() {
+        this.book = null;
+        this.book = new Book(null, this.TITLE, this.CALL_NUMBER, this.BOOK_ID);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testBookIllegalArgumentExceptionTitle() {
+        this.book = null;
+        this.book = new Book(this.AUTHOR, null, this.CALL_NUMBER, this.BOOK_ID);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testBookIllegalArgumentExceptionCallNumber() {
+        this.book = null;
+        this.book = new Book(this.AUTHOR, this.TITLE, null, this.BOOK_ID);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testBookIllegalArgumentExceptionBookID() {
+        this.book = null;
+        this.book = new Book(this.AUTHOR, this.TITLE, this.CALL_NUMBER, 0);
+    }
+    
     @Test
     public void testBook() {
-        fail("Not yet implemented");
+        assertNotNull(this.book);
     }
 
 
