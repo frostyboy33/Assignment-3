195591aa665ab9266e700a4d308996f9bbe66971
Jaan Liiband <jaan@liiband.com>
2015-10-02 11:41:23 +1000

    properly finished the setUp() method with all needed variables.
    modified the tearDown() method to reset all variables

--- src/library/testers/IntegrationTestBorrowUC_CTL.java
+++ src/library/testers/IntegrationTestBorrowUC_CTL.java
@@ -11,6 +11,12 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import library.BorrowUC_CTL;
+import library.entities.BookDAO;
+import library.entities.BookHelper;
+import library.entities.LoanHelper;
+import library.entities.LoanMapDAO;
+import library.entities.MemberHelper;
+import library.entities.MemberMapDAO;
 import library.interfaces.IBorrowUI;
 import library.interfaces.daos.IBookDAO;
 import library.interfaces.daos.ILoanDAO;
@@ -39,9 +45,19 @@ public class IntegrationTestBorrowUC_CTL {
     @Before
     public void setUp() throws Exception {
         
+        this.cardReader = Mockito.mock(ICardReader.class);
+        this.scanner = Mockito.mock(IScanner.class);
+        this.printer = Mockito.mock(IPrinter.class);
+        this.display = Mockito.mock(IDisplay.class);
+        
+        this.bookDAO = new BookDAO(new BookHelper());
+        this.loanDAO = new LoanMapDAO(new LoanHelper());
+        this.memberDAO = new MemberMapDAO(new MemberHelper());
+        
         
         this.borrowUI = Mockito.mock(IBorrowUI.class);
         this.pendingLoanList = new ArrayList<ILoan>();
+        
         this.borrowControl = new BorrowUC_CTL(this.cardReader,
                                               this.scanner,
                                               this.printer,
@@ -57,13 +73,27 @@ public class IntegrationTestBorrowUC_CTL {
 
     @After
     public void tearDown() throws Exception {
+        this.cardReader = null;
+        this.scanner = null;
+        this.printer = null;
+        this.display = null;
+        
+        this.bookDAO = null;
+        this.loanDAO = null;
+        this.memberDAO = null;
+        
+        
+        this.borrowUI = null;
+        this.pendingLoanList = null;
+        
+        this.borrowControl = null;
     }
 
 
 
     @Test
     public void testInitialise() {
-        fail("Not yet implemented");
+        
     }
 
 
