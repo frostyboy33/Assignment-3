d334f76f602f99e5d1613b31a64e5ce147a396cd
Jaan Liiband <jaan@liiband.com>
2015-10-02 14:42:05 +1000

    Added the ability for test data to be generated.
    Added the testCardSwiped() method to test a clean use case

--- src/library/testers/IntegrationTestBorrowUC_CTL.java
+++ src/library/testers/IntegrationTestBorrowUC_CTL.java
@@ -3,6 +3,8 @@ package library.testers;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.swing.JPanel;
@@ -25,7 +27,9 @@ import library.interfaces.IBorrowUI;
 import library.interfaces.daos.IBookDAO;
 import library.interfaces.daos.ILoanDAO;
 import library.interfaces.daos.IMemberDAO;
+import library.interfaces.entities.IBook;
 import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
 import library.interfaces.hardware.ICardReader;
 import library.interfaces.hardware.IDisplay;
 import library.interfaces.hardware.IPrinter;
@@ -46,7 +50,13 @@ public class IntegrationTestBorrowUC_CTL {
     
     BorrowUC_CTL borrowControl;
     
-    private final Integer MEMBER_ID = 1;
+    private final Integer MEMBER_CLEAN = 1;
+    private final Integer MEMBER_OVERDUE = 2;
+    private final Integer MEMBER_MAXED_FINES = 3;
+    private final Integer MEMBER_MAXED_LOANS = 4;
+    private final Integer MEMBER_ACCEPTABLE_FINES = 5;
+    private final Integer MEMBER_ACCEPTABLE_LOANS = 6;
+    
     
 
     @Before
@@ -58,10 +68,10 @@ public class IntegrationTestBorrowUC_CTL {
         this.display = Mockito.mock(IDisplay.class);
         
         
-        this.bookDAO = new BookDAO(new BookHelper());
+        this.bookDAO = new BookDAO(new BookHelper());      
+        this.memberDAO = new MemberMapDAO(new MemberHelper());      
         this.loanDAO = new LoanMapDAO(new LoanHelper());
-        this.memberDAO = new MemberMapDAO(new MemberHelper());
-        
+        this.setupTestData();
         
         this.borrowUI = Mockito.mock(ABorrowPanel.class);
         this.pendingLoanList = new ArrayList<ILoan>();
@@ -77,6 +87,67 @@ public class IntegrationTestBorrowUC_CTL {
                                               this.pendingLoanList);
         
         
+    }
+    
+    
+    
+    private void setupTestData() {
+        IBook[] book = new IBook[15];
+        IMember[] member = new IMember[6];
+        
+        book[0]  = bookDAO.addBook("author1", "title1", "callNo1");
+        book[1]  = bookDAO.addBook("author1", "title2", "callNo2");
+        book[2]  = bookDAO.addBook("author1", "title3", "callNo3");
+        book[3]  = bookDAO.addBook("author1", "title4", "callNo4");
+        book[4]  = bookDAO.addBook("author2", "title5", "callNo5");
+        book[5]  = bookDAO.addBook("author2", "title6", "callNo6");
+        book[6]  = bookDAO.addBook("author2", "title7", "callNo7");
+        book[7]  = bookDAO.addBook("author2", "title8", "callNo8");
+        book[8]  = bookDAO.addBook("author3", "title9", "callNo9");
+        book[9]  = bookDAO.addBook("author3", "title10", "callNo10");
+        book[10] = bookDAO.addBook("author4", "title11", "callNo11");
+        book[11] = bookDAO.addBook("author4", "title12", "callNo12");
+        book[12] = bookDAO.addBook("author5", "title13", "callNo13");
+        book[13] = bookDAO.addBook("author5", "title14", "callNo14");
+        book[14] = bookDAO.addBook("author5", "title15", "callNo15");
+        
+        member[0] = memberDAO.addMember("fName0", "lName0", "0001", "email0");
+        member[1] = memberDAO.addMember("fName1", "lName1", "0002", "email1");
+        member[2] = memberDAO.addMember("fName2", "lName2", "0003", "email2");
+        member[3] = memberDAO.addMember("fName3", "lName3", "0004", "email3");
+        member[4] = memberDAO.addMember("fName4", "lName4", "0005", "email4");
+        member[5] = memberDAO.addMember("fName5", "lName5", "0006", "email5");
+        
+        Calendar cal = Calendar.getInstance();
+        Date now = cal.getTime();
+                
+        //create a member with overdue loans        
+        for (int i=0; i<2; i++) {
+            ILoan loan = loanDAO.createLoan(member[1], book[i]);
+            loanDAO.commitLoan(loan);
+        }
+        cal.setTime(now);
+        cal.add(Calendar.DATE, ILoan.LOAN_PERIOD + 1);
+        Date checkDate = cal.getTime();     
+        loanDAO.updateOverDueStatus(checkDate);
+        
+        //create a member with maxed out unpaid fines
+        member[2].addFine(10.0f);
+        
+        //create a member with maxed out loans
+        for (int i=2; i<7; i++) {
+            ILoan loan = loanDAO.createLoan(member[3], book[i]);
+            loanDAO.commitLoan(loan);
+        }
+        
+        //a member with a fine, but not over the limit
+        member[4].addFine(5.0f);
+        
+        //a member with a couple of loans but not over the limit
+        for (int i=7; i<9; i++) {
+            ILoan loan = loanDAO.createLoan(member[5], book[i]);
+            loanDAO.commitLoan(loan);
+        }
     }
 
 
@@ -118,12 +189,14 @@ public class IntegrationTestBorrowUC_CTL {
 
     @Test(expected = RuntimeException.class)
     public void testCardSwipedRuntimeException() {
-        this.borrowControl.cardSwiped(this.MEMBER_ID);
+        this.borrowControl.cardSwiped(this.MEMBER_CLEAN);
     }
 
     @Test
     public void testCardSwiped() {
-        fail("Not yet implemented");
+        this.borrowControl.initialise();
+        this.borrowControl.cardSwiped(this.MEMBER_CLEAN);
+        assertEquals(EBorrowState.SCANNING_BOOKS, this.borrowControl.getState());
     }
 
 }
